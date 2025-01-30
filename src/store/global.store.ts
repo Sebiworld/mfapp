7fa47e8a9423e7a3d7a951f9d93b3ddc5b591572
Sebastian Schendel <sebi@sebi.dev>
2025-01-30 22:19:41 +0100

    Remove store persist

--- src/store/global.store.ts
+++ src/store/global.store.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-import { devtools, persist } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 import {
   createInitializationSlice,
   InitializationSlice,
@@ -16,14 +16,14 @@ export type GlobalStore = InitializationSlice &
 
 export const useGlobalStore = create<GlobalStore>()(
   devtools(
-    persist(
-      immer((...a) => ({
-        ...createInitializationSlice(...a),
-        ...createAuthSlice(...a),
-        ...createPagesSlice(...a),
-        ...createSettingsSlice(...a),
-      })),
-      { name: "mfStore" }
-    )
+    // persist(
+    immer((...a) => ({
+      ...createInitializationSlice(...a),
+      ...createAuthSlice(...a),
+      ...createPagesSlice(...a),
+      ...createSettingsSlice(...a),
+    }))
+    // { name: "mfStore" }
+    // )
   )
 );

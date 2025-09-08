66273cbcf44fcc16b27fb5333b563964d0d8579c
Sebastian Schendel <sebi@sebi.dev>
2025-09-08 08:00:51 +0200

    Remove linter warning

--- src/components/contentBlocks/variants/contentGallery/galleryTypes/contentGallerySlider/components/ContentGallerySliderItem.tsx
+++ src/components/contentBlocks/variants/contentGallery/galleryTypes/contentGallerySlider/components/ContentGallerySliderItem.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@mui/material";
 import React from "react";
 import { ImageDto } from "@models/image-dto.model";
 import { LazyPicture } from "@components/lazyPicture/LazyPicture";
